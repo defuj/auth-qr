24e29f136876d63561558b81c3fe457a765d466b
Dede Fuji Abdul <defuj.official@gmail.com>
2025-01-03 22:53:28 +0700

    remove service worker update logic and PWA configuration from App.vue and vite.config.ts

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 import { NotificationProvider } from '@/components/Notification'
-import { onMounted, ref, watch } from 'vue'
 import { useDialogStore } from '@/stores/dialog'
 import DialogProgress from '@/components/Dialogs/DialogProgress.vue'
 import DialogProvider from '@/components/Dialogs/DialogProvider.vue'
 import BasicAction from '@/components/Dialogs/BasicAction.vue'
 const dialog = useDialogStore()
-
-const showUpdate = ref(false)
-
-const refreshApp = () => {
-  navigator.serviceWorker.getRegistration().then((registration) => {
-    if (registration?.waiting) {
-      registration.waiting.postMessage({ type: 'SKIP_WAITING' })
-      window.location.reload()
-    }
-  })
-}
-
-watch(
-  () => showUpdate.value,
-  (value) => {
-    if (value) {
-      refreshApp()
-    }
-  }
-)
-
-onMounted(async () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.addEventListener('controllerchange', () => {
-      // Reload halaman saat service worker baru aktif
-      window.location.reload()
-    })
-
-    navigator.serviceWorker.ready.then((registration) => {
-      if (registration?.waiting) {
-        showUpdate.value = true
-      }
-    })
-  }
-})
 </script>
 
 <template>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,42 +13,7 @@ export default defineConfig({
         }
       }
     }),
-    vueJsx(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Human Resource Information System',
-        short_name: 'HRIS',
-        description: 'Human Resource Information System for DuniaBuah',
-        theme_color: '#FCF6EC',
-        background_color: '#FFFFFF',
-        start_url: '.',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'assets/images/favicon.ico',
-            sizes: '64x64 32x32 24x24 16x16',
-            type: 'image/x-icon'
-          },
-          {
-            src: 'assets/images/logo192.png',
-            type: 'image/png',
-            sizes: '192x192'
-          },
-          {
-            src: 'assets/images/logo512.png',
-            type: 'image/png',
-            sizes: '512x512'
-          }
-        ]
-      },
-      workbox: {
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-        skipWaiting: true,
-        maximumFileSizeToCacheInBytes: 5000000
-      }
-    })
+    vueJsx()
   ],
   resolve: {
     alias: {
